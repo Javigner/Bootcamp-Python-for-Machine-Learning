5ab0403708907172599206d25f6cd8f407cc93e6
Jacky VIGNERON <javigner@e1r5p18.42.fr>
2019-11-06 17:45:31 +0100

    ft_reduce

--- Day02/ex00/ft_reduce.py
+++ Day02/ex00/ft_reduce.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  6 17:24:09 2019
+import sys
 
-@author: javigner
-"""
-
+def ft_reduce(function_to_apply, list_of_inputs):
+    if (isinstance(list_of_inputs, list) == False and isinstance(list_of_inputs, range) == False):
+        sys.exit("list_of_inputs must be a list")
+    if (callable(function_to_apply) == False):
+        sys.exit("function_to_aply must be a function")
+    result = list_of_inputs[0]
+    i = 1
+    while(i < len(list_of_inputs)):
+        result = function_to_apply(result, list_of_inputs[i])
+        i += 1
+    return result
