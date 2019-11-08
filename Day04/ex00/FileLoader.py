92a5cdc5b518d5108ce972422c883e4f6f97e40a
Jacky VIGNERON <javigner@e1r12p18.42.fr>
2019-11-08 14:01:02 +0100

    FilerLoader

--- Day04/ex00/FileLoader.py
+++ Day04/ex00/FileLoader.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  8 13:28:13 2019
+import pandas as pd
 
-@author: javigner
-"""
+class Filerloader:
+    def load(path):
+        df = pd.read_csv(path)
+        size = df.shape
+        print("Loading dataset of dimensions " + str(size[0]) + ' x ' + str(size[1]))
+        return df
+    def display(df, n):
+        if n >= 0:
+            print (df.head(n))
+        if n < 0:
+            print(df.tail(-n))
+            
 
+df = Filerloader.load("FL_insurance_sample.csv")
+Filerloader.display(df, 0)
